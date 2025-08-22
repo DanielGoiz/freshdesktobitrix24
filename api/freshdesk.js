52c0ab1e6ce3860760e4aaac88b5bbbc9d72e160
Daniel Gois <150284833+DanielGoiz@users.noreply.github.com>
2025-08-22 16:03:50 -0300

    Update freshdesk.js

--- api/freshdesk.js
+++ api/freshdesk.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BITRIX_WEBHOOK_URL = "https://alfanexus.bitrix24.com.br/rest/13/g85cgof62pmxtmtt/tasks.task.add";
+const BITRIX_WEBHOOK_URL = "https://alfanexus.bitrix24.com.br/rest/13/pxv2w31pfrpuk2oe/tasks.task.add";
 const RESPONSIBLE_ID = 13; // ID do usuário responsável na sua conta Bitrix24
 const DEADLINE_DAYS = 3; // prazo padrão em dias
 const MAX_RETRIES = 3; // número de tentativas em caso de erro temporário
